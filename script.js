907046d454ca8c2e67d116679286ce3623532427
gustavwiese <gusse12345@gmail.com>
2023-04-17 10:31:52 +0200

    added getPosts function

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 "use strict";
 
+const endpoint = "https://first-firebase-21c4d-default-rtdb.europe-west1.firebasedatabase.app/";
+
 window.addEventListener("load", start);
 
 function start() {
   console.log("JavaScript Kører");
-  getData();
+  getPosts();
 }
 
-async function getData(){
-    
+async function getPosts() {
+  const response = await fetch(`${endpoint}/posts.json`);
+  const data = await response.json();
+  console.log(data);
 }
+
+
